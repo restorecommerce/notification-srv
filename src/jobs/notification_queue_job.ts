f905b18cff58b89debdc1d1ce564fcb206114858
akumar <arun.kumar@n-fuse.co>
2024-03-01 11:34:45 +0100

    fix: up queue name

--- src/jobs/notification_queue_job.ts
+++ src/jobs/notification_queue_job.ts
@@ -2,7 +2,7 @@ import { processPendingNotifications } from './implementation/process_pending_no
 import { PROCESS_PENDING_NOTIFICATIONS } from '../service.js';
 
 export default async (cfg, logger, events, service, runWorker) => {
-  await runWorker('notificationSrvQueue', 1, cfg, logger, events, async (job) => {
+  await runWorker('notification-srv-queue', 1, cfg, logger, events, async (job) => {
     if (job.type === PROCESS_PENDING_NOTIFICATIONS) {
       await processPendingNotifications(service, logger);
     }
